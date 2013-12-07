38191fb4849510f4a7a42cae7ac3e550639201c2
ppoulsen <ppoulsen21@gmail.com>
2013-12-07 13:51:14 -0600

    Solved problem #1: percentWalls(Map).

---
+++ csce322a3p1.pl
@@ -0,0 +1,43 @@
+isBorder(b).
+isWall(w).
+isFood(f).
+isPower(p).
+isMan(m).
+isGhost(g).
+isGhostFood(6).
+isGhostPower(r).
+isEmpty(u).
+
+wallCountRow([], RowCount) :- RowCount is 0.
+wallCountRow([w|T], RowCount) :-
+    wallCountRow(T, RemainingRowCount),
+    RowCount is (1 + RemainingRowCount).
+wallCountRow([_|T], RowCount) :-
+    wallCountRow(T, RemainingRowCount),
+    RowCount is RemainingRowCount.
+
+wallCount([], WallCount) :- WallCount is 0.
+wallCount([H|T], WallCount) :-
+    wallCountRow(H, RowCount),
+    wallCount(T, RestOfWallCount),
+    WallCount is (RowCount + RestOfWallCount).
+
+cellCountRow([], RowCount) :- RowCount is 0.
+cellCountRow([_|T], RowCount) :-
+    cellCountRow(T, RemainingRowCount),
+    RowCount is (1 + RemainingRowCount).
+
+cellCount([], CellCount) :- CellCount is 0.
+cellCount([H|T], CellCount) :-
+    cellCountRow(H, RowCount),
+    cellCount(T, RestOfCellCount),
+    CellCount is (RowCount + RestOfCellCount).
+
+percentWallsCalc(Map, X) :-
+    wallCount(Map, WallCount),
+    cellCount(Map, CellCount),
+    X is (float(WallCount) / float(CellCount)).
+
+percentWalls(Map) :-
+    percentWallsCalc(Map, X),
+    X @>= 0.10.
