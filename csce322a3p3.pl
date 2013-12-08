769912a1bea14ee335b5b30744c144fabf3a7ba4
ppoulsen <ppoulsen21@gmail.com>
2013-12-07 20:44:40 -0600

    Added power cell check.

--- csce322a3p3.pl
+++ csce322a3p3.pl
@@ -4,6 +4,24 @@
 allBorders([]).
 allBorders([H|T]) :- H == b, allBorders(T).
 
+isCell(b).
+isCell(w).
+isCell(f).
+isCell(p).
+isCell(m).
+isCell(g).
+isCell(6).
+isCell(r).
+isCell(u).
+
+validCellsRow([]).
+validCellsRow([H|T]) :- isCell(H), validCellsRow(T).
+
+validCells([]).
+validCells([H|T]) :-
+  validCellsRow(H),
+  validCells(T).
+
 checkEdges([]).
 checkEdges([H|T]) :-
     nth0(0, H, FirstCell),
@@ -22,8 +40,25 @@ checkBorders(Map) :-
     allBorders(FirstRow),
     allBorders(LastRow),
     checkEdges(Map).
+
+checkPowerCorner(Row) :-
+    length(Row, Cellcount),
+    nth0(1, Row, SecondCell),
+    SndLstCellInd is Cellcount - 2,
+    nth0(SndLstCellInd, Row, SndLastCell),
+    ( SecondCell == p ; SecondCell == r ),
+    ( SndLastCell == p ; SndLastCell == r).
     
+powerCells(Map) :-
+    length(Map, Rowcount),
+    nth0(1, Map, SecondRow),
+    SndLstRowInd is Rowcount - 2,
+    nth0(SndLstRowInd, Map, SndLastRow),
+    checkPowerCorner(SecondRow),
+    checkPowerCorner(SndLastRow).
 
 makeMap(Map) :-
+    validCells(Map),
     percentWalls(Map),
-    checkBorders(Map).
+    checkBorders(Map),
+    powerCells(Map).
